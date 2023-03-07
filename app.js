f8b4d70326359efa97cabfe043fe53e395253015
Saret <117845928+Saret011@users.noreply.github.com>
2023-03-06 21:36:52 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+let boton = document.getElementById('sumar');
+let boton2 = document.getElementById('restar');
+let boton3 = document.getElementById('dividir');
+let boton4 = document.getElementById('multiplicar');
+
+let respuesta = document.getElementById('respuesta');
+
+boton.addEventListener('click',hacersuma);
+
+function hacersuma(){
+    let n1 = parseFloat(document.getElementById('n1').value);
+    let n2 = parseFloat(document.getElementById('n2').value);
+    let n3 = n1+n2;
+    respuesta.innerHTML = `El resultado de la suma es: ${n3}`;
+}
+
+boton2.addEventListener('click',haceresta);
+
+function haceresta(){
+    let n1 = parseFloat(document.getElementById('n1').value);
+    let n2 = parseFloat(document.getElementById('n2').value);
+    let n3 = n1-n2;
+    respuesta.innerHTML = `El resultado de la resta es: ${n3}`;
+}
+
+boton3.addEventListener('click',hacerdivision);
+
+function hacerdivision(){
+    let n1 = parseFloat(document.getElementById('n1').value);
+    let n2 = parseFloat(document.getElementById('n2').value);
+    let n3 = n1/n2;
+    respuesta.innerHTML = `El resultado de la division es: ${n3}`;
+}
+
+boton4.addEventListener('click',hacermultiplicacion);
+
+function hacermultiplicacion(){
+    let n1 = parseFloat(document.getElementById('n1').value);
+    let n2 = parseFloat(document.getElementById('n2').value);
+    let n3 = n1*n2;
+    respuesta.innerHTML = `El resultado de la multiplicacion es: ${n3}`;
+}
